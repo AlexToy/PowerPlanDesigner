4880c4e9ba19c276339ff5db06c16e89bb35c6f3
AlexHaristoy <alexandre.haristoy@gmail.com>
2022-04-08 15:09:31 +0200

    ???

--- GraphicsScene.py
+++ GraphicsScene.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QGraphicsScene, QApplication, QGraphicsRectItem, QGraphicsRectItem, QRubberBand
-from PyQt5.QtCore import QPointF, Qt, QRectF, QSize, QRect
-from PyQt5.QtGui import QTransform, QPainterPath, QPainter
+from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtCore import QPointF, Qt
+from PyQt5.QtGui import QTransform
+from PyQt5.QtWidgets import QApplication
 
-SPEED_MOVE_FACTOR = 0.6
+SPEED_MOVE_FACTOR = 0.4
 
 
 class GraphicsScene(QGraphicsScene):
@@ -13,20 +14,19 @@ class GraphicsScene(QGraphicsScene):
         self.move_scene_locked = True
         self.move_item_locked = True
         self.item = None
+        self.orig_cursor_position = 0
+        self.orig_scene_position = 0
 
     def mousePressEvent(self, event):
         self.item = self.itemAt(event.scenePos(), QTransform())
 
         if event.button() == Qt.LeftButton:
-            # Left click --> Move an item on the scene
             self.move_scene_locked = True
             self.move_item_locked = False
             if self.item is not None:
-                self.setFocusItem(self.item)
                 self.item.item_clicked_from_scene()
 
         elif event.button() == Qt.RightButton:
-            # Right click --> Move the view on the scene
             self.move_scene_locked = False
             self.move_item_locked = True
             self.orig_cursor_position = event.lastScenePos()
@@ -37,6 +37,9 @@ class GraphicsScene(QGraphicsScene):
 
         # Move an item on the scene
         if self.item is not None and not self.move_item_locked:
+
+            orig_cursor_position = event.lastScenePos()
+            updated_cursor_position = event.scenePos()
             orig_item_position = self.item.scenePos()
             updated_cursor_x = updated_cursor_position.x() - orig_cursor_position.x() + orig_item_position.x()
             updated_cursor_y = updated_cursor_position.y() - orig_cursor_position.y() + orig_item_position.y()
@@ -44,7 +47,7 @@ class GraphicsScene(QGraphicsScene):
             self.item.item_moved_from_scene(updated_cursor_x, updated_cursor_y)
 
         # Move the view of the scene
-        if not self.move_scene_locked:
+        if self.item is None and not self.move_scene_locked:
             QApplication.changeOverrideCursor(Qt.ClosedHandCursor)
             updated_cursor_position = event.scenePos()
             print(updated_cursor_position)
